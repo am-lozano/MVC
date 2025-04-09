5a9d9dd536806e0feb89484a8cdead0bea87d6e9
am-lozano <am.lozano@iesnervion.es>
2025-04-09 12:56:20 +0200

    comprobar vista y terminarla, arreglar index

---
+++ Vista/Vista.js
@@ -0,0 +1,53 @@
+class Vista {
+
+    // constructor
+    constructor() {
+        this.form = document.getElementById("#");
+        this.formInput = document.getElementById("#");
+        this.lista = document.getElementById("#");
+        this.boton = document.getElementById("#");
+    }
+
+    // devolvemos el formulario
+    getInput() {
+        return this.formInput.value;
+    }
+
+    // funcion para limpiar el texto anterior del formulario
+    limpiarForm() {
+        this.formInput.value = "";
+    }
+
+    // funcion para mostrar los jugadores
+    renderizarJug(jugador) {
+        this.lista.innerHTML = "";
+        jugador.array.forEach(element => {
+            const fila = document.createElement("tr");
+            fila.innerHTML = `
+            <td> ${element.nombre} </td>
+            <td> ${element.edad} </td>
+            <td> ${element.posicion} </td>
+            <td> ${element.fechanacimiento} </td>
+            <td> <button class="success-btn" data-id="#{element.id}"> Asignar Equipo </button> </td>
+            <td> <button class="success-btn" data-id="#{element.id}"> Eliminar Jugador </button> </td>
+            `;
+            this.lista.appendChild(fila);
+        });
+    }
+
+    // funcion para mostrar los equipos
+    renderizarEquipo(equipo) {
+        this.lista.innerHTML = "";
+        equipo.array.forEach(element => {
+            const fila = document.createElement("tr");
+            fila.innerHTML = `
+            <td> ${element.nombre} </td>
+            <td> ${element.ciudad} </td>
+            <td> ${element.estadio} </td>
+            <td> <button class="success-btn" data-id="#{element.id}"> Agregar Jugador </button> </td>
+            <td> <button class="success-btn" data-id="#{element.id}"> Eliminar Jugador </button> </td>
+            `;
+            this.lista.appendChild(fila);
+        });
+    }
+}
